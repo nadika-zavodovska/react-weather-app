48235af68f04cf3b4f91a936986962936bbf0920
Nadika <nadika.name@gmail.com>
2024-06-26 16:26:33 +0100

    Adding header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 import Form from './Form';
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
+    <h1>Forecast</h1>
       <Form />
+
     </div>
   );
 }
